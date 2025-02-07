86e10855db3279783b3185c6e273cd7b45ce14df
smallsilverwest <smallsilverwest@ewha.ac.kr>
2025-02-08 00:30:55 +0900

    Feat: 프로젝트 초기 설정

---
+++ src/main/java/ecc_2024/sogonsogon/SogonsogonApplication.java
@@ -0,0 +1,13 @@
+package ecc_2024.sogonsogon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogonsogonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SogonsogonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecc_2024/sogonsogon/SogonsogonApplicationTests.java
@@ -0,0 +1,13 @@
+package ecc_2024.sogonsogon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogonsogonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
