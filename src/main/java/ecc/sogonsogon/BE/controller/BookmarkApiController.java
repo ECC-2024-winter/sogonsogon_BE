814daa3f3f5315a094c74e9904c011b505ace0d3
goodispotato <potatoisgood@ewhain.net>
2025-02-21 15:25:33 +0900

    feat: Delete bookmarks

--- src/main/java/ecc/sogonsogon/BE/controller/BookmarkApiController.java
+++ src/main/java/ecc/sogonsogon/BE/controller/BookmarkApiController.java
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
@@ -38,5 +35,19 @@ public class BookmarkApiController {
         return (saved!=null) ?
                 ResponseEntity.status(HttpStatus.OK).body(saved) :
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @DeleteMapping("/bookmarks/{id}")
+    public ResponseEntity<Bookmark> delete(@RequestHeader("Authorization") String token, @PathVariable Integer id) {
+
+        String jwt = token.substring(7);
+        String email = jwtUtil.extractEmail(jwt);
+
+        User user = userService.findUserByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"해당 유저를 찾을 수 없습니다."));
+
+        Bookmark deleted = bookmarkService.delete(user, id);
+        return (deleted!=null)?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() : //북마크가 있다면 삭제됐으니 no content
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); //북마크가 없다면 삭제 못했다는 뜻
     }
 }
