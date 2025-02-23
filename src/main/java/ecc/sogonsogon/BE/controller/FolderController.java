9b1e4e4e6d51df4e43e719a4d95c3679b5df4b62
smallsilverwest <smallsilverwest@ewha.ac.kr>
2025-02-23 23:13:35 +0900

    Chore: Jwt 경로 및 method 수정

--- src/main/java/ecc/sogonsogon/BE/controller/FolderController.java
+++ src/main/java/ecc/sogonsogon/BE/controller/FolderController.java
@@ -3,11 +3,11 @@ package ecc.sogonsogon.BE.controller;
 import ecc.sogonsogon.BE.entity.Bookmark;
 import ecc.sogonsogon.BE.entity.Folder;
 import ecc.sogonsogon.BE.entity.User;
-import ecc.sogonsogon.BE.jwt.JwtTokenProvider;
+import ecc.sogonsogon.BE.security.JwtUtil;
 import ecc.sogonsogon.BE.repository.FolderRepository;
-import ecc.sogonsogon.BE.service.UserService;
 import ecc.sogonsogon.BE.service.FolderService;
 import ecc.sogonsogon.BE.service.BookmarkService;
+import ecc.sogonsogon.BE.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,9 +24,9 @@ import java.util.List;
 @Controller
 public class FolderController {
     @Autowired
-    private JwtTokenProvider jwtUtil;
+    private JwtUtil jwtUtil;
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
     @Autowired
     private FolderRepository folderRepository;
     @Autowired
@@ -39,7 +39,7 @@ public class FolderController {
         String jwt = token.substring(7);
         String email = jwtUtil.extractEmail(jwt);
 
-        User user = userService.findUserByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"해당 유저를 찾을 수 없습니다."));
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"해당 유저를 찾을 수 없습니다."));
         //1. 모든 폴더 데이터 가져오기
         List<Folder> folderEntityList = folderService.showFolders(user);
         //2. 모델에 데이터 등록하기
@@ -53,7 +53,7 @@ public class FolderController {
         String jwt = token.substring(7);
         String email = jwtUtil.extractEmail(jwt);
 
-        User user = userService.findUserByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"해당 유저를 찾을 수 없습니다."));
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"해당 유저를 찾을 수 없습니다."));
         //1. id 조회해 데이터 가져오기
         Folder folderEntity = folderRepository.findByUserAndFolderId(user,id).orElse(null);
         List<Bookmark> bookmarks = bookmarkService.showBookmarksInFolder(user,folderEntity);
